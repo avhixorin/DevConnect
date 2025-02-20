384d1e937c1872ed7488f5de631dc74c06b425c2
avhixorin <avhixorin@gmail.com>
2025-02-20 17:24:15 +0530

    Enhance Hero component styling and update layout for improved visual appeal

--- frontend/src/components/LandingPage/Hero/Hero.tsx
+++ frontend/src/components/LandingPage/Hero/Hero.tsx
@@ -1,6 +1,8 @@
+
 import React from "react";
 import ShinyText from "../../Bits/ShinyText/ShinyText";
 import Threads from "../../Bits/HyperSpeed/Threads";
+
 
 const Hero: React.FC = () => {
   return (
@@ -12,7 +14,6 @@ const Hero: React.FC = () => {
         <Threads />
       </div>
 
-      {/* Content */}
       <div className="relative z-10 flex flex-col items-center">
         <ShinyText
           text="Announcing our new product"
@@ -21,8 +22,11 @@ const Hero: React.FC = () => {
           className="border text-base border-[#242441] px-4 py-1 text-slate-500 transition duration-300 rounded-full"
         />
 
-        <h1 className="text-center text-7xl md:text-8xl font-medium leading-tight text-white mt-6">
-          DevConnect is the new <br /> standard for collaboration
+        <h1 className="text-center text-7xl md:text-8xl font-medium leading-tight text-white flex flex-col gap-4 mt-6">
+          DevConnect is the new <br />
+          <span className="bg-gradient-to-r from-blue-400 via-pink-500 via-yellow-500 to-slate-400 bg-clip-text text-transparent">
+            standard for collaboration
+          </span>
         </h1>
 
         <div className="w-full text-center mt-4 p-4 shadow-md rounded-lg">
