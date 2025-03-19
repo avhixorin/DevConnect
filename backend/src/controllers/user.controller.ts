cabed1a1f9b2b9bd23e9b76207f625cb583bc079
avhixorin <avhixorin@gmail.com>
2025-03-19 15:26:13 +0530

    Enhance user model and implement JWT authentication; add access and refresh token generation, update login validation

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import bcrypt from "bcryptjs";
 import prisma from "../prisma";
+import jwt from "jsonwebtoken";
 
 const register = async (req: Request, res: Response) => {
   const errors = validationResult(req);
@@ -19,6 +20,7 @@ const register = async (req: Request, res: Response) => {
       return;
     }
     const hashedPassword = await bcrypt.hash(password, 10);
+    
     const newUser = await prisma.user.create({
       data: {
         username,
@@ -27,19 +29,62 @@ const register = async (req: Request, res: Response) => {
       },
     });
 
-    res.status(201).json({ message: "User registered successfully", user: newUser });
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+const generateAccessTokenAndRefreshToken = async (id: string) => {
+  const accessToken = jwt.sign(
+    { userId: id },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "15m" }
+  );
 
-  if (!email || !password) {
-    res.status(400).json({ message: "Please fill all fields" });
+  const refreshToken = jwt.sign(
+    { userId: id },
+    process.env.JWT_REFRESH_SECRET as string,
+    { expiresIn: "7d" }
+  );
+
+  return { accessToken, refreshToken };
+};
+
+const login = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
   }
+
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    res.status(404).json({ message: "Email not found" });
+    return;
+  }
+  
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  console.log("isPasswordCorrect", isPasswordCorrect)
+
+  if (!isPasswordCorrect) {
+    res.status(400).json({ message: "Incorrect password" });
+    return;
+  }
+  const { accessToken, refreshToken } =
+    await generateAccessTokenAndRefreshToken(user.id);
+  res
+    .cookie("refreshToken", refreshToken, { httpOnly: true, secure: true })
+    .cookie("accessToken", accessToken, { httpOnly: true, secure: true });
 
   console.log("User logged in", email, password);
   res.status(200).json({ message: "User logged in" });
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -15,6 +15,13 @@ router.post(
   ],
   register
 );
-router.post("/login", login);
+router.post("/login",
+  [
+    body("email").isEmail().withMessage("Valid email is required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  login);
 
 export default router;
